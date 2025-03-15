a854a753ed21544e5fdc18a262f1e40d90d15f9c
Antonio Kim <antonio_kim@outlook.com>
2025-03-14 17:14:19 -0700

    replacing flake8 with pylint and setting tox fail-under

--- src/connections/__init__.py
+++ src/connections/__init__.py
@@ -1,5 +1,3 @@
 """Simple Machine-Learning app solving New York Times' Connections"""
 
 __version__ = "0.0.1"
-
-from .cli import app  # noqa
--- tests/conftest.py
+++ tests/conftest.py
@@ -9,6 +9,7 @@ runner = CliRunner()
 @pytest.fixture()
 def connections_cli():
     """Fixture to run Connections CLI and return results"""
+
     def invoke(commands=""):
         result = runner.invoke(app, commands.split())
         return result
