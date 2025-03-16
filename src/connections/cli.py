824f2b4270c34831970723dc5efb6482d99bd6d3
Antonio Kim <antonio_kim@outlook.com>
2025-03-16 16:51:59 -0700

    adding happy case test with randomized solve function

--- src/connections/cli.py
+++ src/connections/cli.py
@@ -1,5 +1,8 @@
 import typer
+from typing import List
+
 from . import __version__
+from . import api
 
 app = typer.Typer()
 
@@ -14,6 +17,24 @@ def version():
 def status():
     """Returns ok on the command line when invoked"""
     print("Status: OK")
+
+
+@app.command()
+def solve(words: List[str] = typer.Argument(..., help="16 words from Connections")):
+    """
+    Returns estimated lists of words in group based on the
+    input. Based on ML model used to solve the connections.
+    Must be exactly 16 words
+    """
+
+    if len(words) != 16:
+        typer.echo("Error: Input must have 16 words after solve command", err=True)
+        raise typer.Exit(code=1)
+
+    line_output = api.solve_model(words)
+
+    for line in line_output:
+        typer.echo(" ".join(line))
 
 
 if __name__ == "__main__":
--- tests/test_prints.py
+++ tests/test_prints.py
@@ -1,5 +1,6 @@
 def test_prints_happy(connections_cli, mock_words):
-    result = connections_cli("solve", mock_words)
+    inputs = " ".join(mock_words)
+    result = connections_cli(f"solve {inputs}")
     assert result.exit_code == 0
     lines = result.stdout.strip().split("\n")
     assert len(lines) == 4
