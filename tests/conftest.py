8d5826e32bd25a192f31a8d0bf5375baa2c6f92f
Antonio Kim <antonio_kim@outlook.com>
2025-03-18 22:58:14 -0700

    finishing up cli

--- tests/conftest.py
+++ tests/conftest.py
@@ -30,6 +30,52 @@ def mock_words():
 
 
 @pytest.fixture()
+def mock_less_words():
+    """Mock words used for testing solve command on the cli"""
+    return [
+        "apple",
+        "banana",
+        "cherry",
+        "date",
+        "elderberry",
+        "fig",
+        "grape",
+        "honeydew",
+        "kiwi",
+        "lemon",
+        "mango",
+        "nectarine",
+        "orange",
+        "pear",
+        "quince",
+    ]
+
+
+@pytest.fixture()
+def mock_more_words():
+    """Mock words used for testing solve command on the cli"""
+    return [
+        "apple",
+        "banana",
+        "cherry",
+        "date",
+        "elderberry",
+        "fig",
+        "grape",
+        "honeydew",
+        "kiwi",
+        "lemon",
+        "mango",
+        "nectarine",
+        "orange",
+        "pear",
+        "quince",
+        "raspberry",
+        "raspberry",
+    ]
+
+
+@pytest.fixture()
 def connections_cli():
     """Fixture to run Connections CLI and return results"""
 
--- tests/test_prints.py
+++ tests/test_prints.py
@@ -9,3 +9,15 @@ def test_prints_happy(connections_cli, mock_words):
 def test_prints_empty(connections_cli):
     result = connections_cli("solve ")
     assert result.exit_code == 1
+
+
+def test_prints_more(connections_cli, mock_more_words):
+    inputs = " ".join(mock_more_words)
+    result = connections_cli(f"solve {inputs}")
+    assert result.exit_code == 1
+
+
+def test_prints_more(connections_cli, mock_less_words):
+    inputs = " ".join(mock_less_words)
+    result = connections_cli(f"solve {inputs}")
+    assert result.exit_code == 1
